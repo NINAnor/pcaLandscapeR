28e571d9e4274c0d9b50283c3bba8f6042079bdf
Reto Spielhofer <reto.spielhofer@t2liprstudio01.nina.no>
2023-09-04 11:18:57 +0200

    test from server

--- R/mod_case_studies.R
+++ R/mod_case_studies.R
@@ -4,28 +4,28 @@
 #'
 #' @param id,input,output,session Internal parameters for {shiny}.
 #'
-#' @noRd 
+#' @noRd
 #'
-#' @importFrom shiny NS tagList 
+#' @importFrom shiny NS tagList
 mod_case_studies_ui <- function(id){
   ns <- NS(id)
   tagList(
- 
+
   )
 }
-    
+
 #' case_studies Server Functions
 #'
-#' @noRd 
+#' @noRd
 mod_case_studies_server <- function(id){
   moduleServer( id, function(input, output, session){
     ns <- session$ns
- 
+
   })
 }
-    
-## To be copied in the UI
+
+# To be copied in the UI
 # mod_case_studies_ui("case_studies_1")
-    
+
 ## To be copied in the server
 # mod_case_studies_server("case_studies_1")
